254f730b4f6bb7fdbd0321f0623be5537d37b524
lalitrnagpal <lalit2801@gmail.com>
2024-09-13 17:50:11 +0530

    some code to commit

---
+++ firstfile.py
@@ -0,0 +1,5 @@
+"""This module has a function that prints odd numbers from 1 to 10"""
+def myfunc():
+    """This is the function that prints odd numbers from 1 to 10"""
+    for i in range(1,10,2):
+        print(i)
